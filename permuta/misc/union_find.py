2b2e0b180393af779c7d303a1a3162febe098639
SuprDewd <suprdewd@gmail.com>
2015-09-09 02:25:30 +0000

    Document UnionFind and implement add function

--- permuta/misc/union_find.py
+++ permuta/misc/union_find.py
@@ -1,19 +1,28 @@
 
 class UnionFind(object):
-    def __init__(self, n):
+    """A collection of distjoint sets."""
+
+    def __init__(self, n = 0):
+        """Creates a collection of n disjoint unit sets."""
         self.p = [-1]*n
         self.leaders = set( i for i in range(n) )
 
     def find(self, x):
+        """Return the identifier of a representative element for the set
+        containing the element with identifier x."""
         if self.p[x] < 0:
             return x
         self.p[x] = self.find(self.p[x])
         return self.p[x]
 
     def size(self, x):
+        """Return the number of elements in the set containing the element with
+        identifier x."""
         return -self.p[self.find(x)]
 
     def unite(self, x, y):
+        """Unite the two sets containing the elements with identifiers x and y,
+        respectively."""
         x = self.find(x)
         y = self.find(y)
         if x == y:
@@ -23,4 +32,11 @@ class UnionFind(object):
         self.p[y] += self.p[x]
         self.p[x] = y
         self.leaders.remove(x)
+
+    def add(self):
+        """Add a unit set containing a new element to the collection, and
+        return the identifier of the new element."""
+        nid = len(self.p)
+        self.p.append(nid)
+        return nid
 
