4bcded8f879b5e3e43b0d0e16027394e337b68d3
SuprDewd <suprdewd@gmail.com>
2015-09-14 17:22:11 +0000

    Add many misc functionality

--- permuta/misc/__init__.py
+++ permuta/misc/__init__.py
@@ -1,2 +1,6 @@
-from .dancing_links import DancingLinks
 from .union_find import UnionFind
+from .misc import flatten, binary_search, choose
+from .dancing_links import DancingLinks
+from .algorithm_x import AlgorithmX
+from .exact_cover import exact_cover
+from .ordered_set_partitions import ordered_set_partitions
---
+++ permuta/misc/misc.py
@@ -0,0 +1,45 @@
+
+import bisect
+
+def binary_search(a, x):
+    i = bisect.bisect_left(a, x)
+    return i != len(a) and a[i] == x
+
+
+def flatten(lst):
+    res = []
+    def dfs(l):
+        for i in l:
+            if type(i) in {list,tuple}:
+                dfs(i)
+            else:
+                res.append(i)
+    dfs(lst)
+    return res
+
+def choose(l, k):
+    cur = []
+    def gen(at, left):
+        if left == 0:
+            yield list(cur)
+        elif at < l:
+            cur.append(at)
+            for res in gen(at + 1, left - 1):
+                yield res
+
+            cur.pop()
+
+            for res in gen(at + 1, left):
+                yield res
+
+    return gen(0, k)
+
+def subsets(elems):
+    def bt(at, cur):
+        if at == len(elems):
+            yield cur
+        else:
+            for x in bt(at+1, cur): yield x
+            for x in bt(at+1, cur + [elems[at]]): yield x
+    for x in bt(0, []): yield x
+
