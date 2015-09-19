23bce1743311a50e35cc04c5e3bbb635110f8c58
SuprDewd <suprdewd@gmail.com>
2015-09-19 15:57:33 +0000

    Added binomial function and moved factorial function

--- permuta/__init__.py
+++ permuta/__init__.py
@@ -4,9 +4,3 @@ from .permutations import Permutations
 from .mesh_pattern import MeshPattern
 from .mesh_patterns import MeshPatterns
 
-def factorial(n):
-    res = 1
-    for i in range(2, n+1):
-        res *= i
-    return res
-
