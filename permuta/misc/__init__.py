5bb3574977df00d77394aa577721dc81db7a282b
SuprDewd <suprdewd@gmail.com>
2015-09-21 23:50:26 +0000

    Add a ProgressBar for entertainment while waiting for tasks

--- permuta/misc/__init__.py
+++ permuta/misc/__init__.py
@@ -4,3 +4,4 @@ from .dancing_links import DancingLinks
 from .algorithm_x import AlgorithmX
 from .exact_cover import exact_cover
 from .ordered_set_partitions import ordered_set_partitions
+from .progressbar import ProgressBar
---
+++ permuta/misc/progressbar.py
@@ -0,0 +1,53 @@
+import time
+import sys
+import math
+
+class ProgressBar(object):
+
+    @staticmethod
+    def create(mx, mn=0):
+        ProgressBar.mn = mn
+        ProgressBar.mx = mx
+        ProgressBar.at = mn
+        ProgressBar.start = time.time()
+        ProgressBar.last = 0
+        sys.stderr.write('\n')
+        ProgressBar.progress(mn)
+
+    @staticmethod
+    def progress(prg=None, fin=False):
+        if prg is not None:
+            ProgressBar.at = prg
+        else:
+            ProgressBar.at = ProgressBar.at + 1
+            prg = ProgressBar.at
+        curt = time.time()
+        if curt - ProgressBar.last < 0.5 and not fin:
+            return
+        ProgressBar.last = curt
+        sys.stderr.write('\033[1F')
+        width = 50
+        prog = float(prg - ProgressBar.mn) / (ProgressBar.mx - ProgressBar.mn)
+        bars = width if ProgressBar.mn == ProgressBar.mx else int(round(prog * width))
+        bars = max(0, min(width, bars))
+        sys.stderr.write('%3d%% [%s%s]' % (round(prog * 100), '#' * bars, '-' * (width - bars)))
+        elapsed = curt - ProgressBar.start
+        # if elapsed >= 4 and prog > 0:
+        show_time = None
+        if fin:
+            show_time = elapsed
+        elif elapsed >= 2 and prog > 0:
+            show_time = max(0, elapsed / prog - elapsed)
+        if show_time is not None:
+            h = math.floor(show_time / 60 / 60)
+            show_time -= h * 60 * 60
+            m = math.floor(show_time / 60)
+            show_time -= m * 60
+            s = math.floor(show_time)
+            sys.stderr.write(' %02d:%02d:%02d' % (h,m,s))
+        sys.stderr.write('\n')
+
+    @staticmethod
+    def finish():
+        ProgressBar.progress(ProgressBar.mx, fin=True)
+
