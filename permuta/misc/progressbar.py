dedd2f21312e36f8f394b890c8261369329b8db2
SuprDewd <suprdewd@gmail.com>
2016-02-12 10:24:05 +0000

    Make progressbar work on Mac

--- permuta/misc/progressbar.py
+++ permuta/misc/progressbar.py
@@ -25,12 +25,13 @@ class ProgressBar(object):
         if curt - ProgressBar.last < 0.5 and not fin:
             return
         ProgressBar.last = curt
-        sys.stderr.write('\033[1F')
+        # sys.stderr.write('\033[1F')
+        sys.stderr.write('\r')
         width = 50
         prog = 1 if ProgressBar.mn == ProgressBar.mx else float(prg - ProgressBar.mn) / (ProgressBar.mx - ProgressBar.mn)
         bars = int(round(prog * width))
         bars = max(0, min(width, bars))
-        sys.stderr.write('%3d%% [%s%s]' % (round(prog * 100), '#' * bars, '-' * (width - bars)))
+        sys.stderr.write('%3d%% [%s%s] ' % (round(prog * 100), '#' * bars, '-' * (width - bars)))
         elapsed = curt - ProgressBar.start
         # if elapsed >= 4 and prog > 0:
         show_time = None
@@ -45,9 +46,10 @@ class ProgressBar(object):
             show_time -= m * 60
             s = math.floor(show_time)
             sys.stderr.write(' %02d:%02d:%02d' % (h,m,s))
-        sys.stderr.write('\n')
+        # sys.stderr.write('\n')
 
     @staticmethod
     def finish():
         ProgressBar.progress(ProgressBar.mx, fin=True)
+        sys.stderr.write('\n')
 
