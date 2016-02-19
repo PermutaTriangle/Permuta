d3af50956823fd4845bb8bbc28ca5b7162cc2037
SuprDewd <suprdewd@gmail.com>
2016-02-19 11:48:15 +0000

    Remove trash

--- build/lib/permuta/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .permutation import Permutation
-from .permutations import Permutations
-from .mesh_pattern import MeshPattern
-from .mesh_patterns import MeshPatterns
-
--- build/lib/permuta/math/__init__.py
+++
@@ -1,1 +0,0 @@
-from .counting import factorial, binomial
