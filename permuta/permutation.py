43976d34609088a72726163cc86943302d4ec12b
RagnarArdal <RagnarArdal@gmail.com>
2016-10-08 16:55:17 +0000

    It be darned as well

--- permuta/permutation.py
+++ permuta/permutation.py
@@ -105,20 +105,6 @@ class Permutation(object):
             k_inc = k+1  # May need to use this lots, so pre-compute
             # Loop over remaining elements of perm (actually i, the index)
             while 1:
-                if elements_left == elements_needed:
-                    # Try to form occurrence with remaining elements
-                    # Avoid making function calls
-                    while i < len(perm):
-                        if predicates[k](perm[i]):
-                            indices[k] = i
-                            k += 1
-                            i += 1
-                        else:
-                            # Failed to form occurrence
-                            return
-                    # Managed to form occurrence
-                    yield indices[:]
-                    return
                 if elements_left < elements_needed:
                     # Can't form an occurrence with remaining elements
                     return
