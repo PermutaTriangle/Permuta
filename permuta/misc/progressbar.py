9b634ba7b38c0314d51537dc2f91cd1b933694e1
Bjarki Ágúst Guðmundsson <suprdewd@gmail.com>
2016-10-25 18:05:03 +0000

    update progressbar

--- permuta/misc/progressbar.py
+++ permuta/misc/progressbar.py
@@ -11,28 +11,28 @@ class ProgressBar(object):
         ProgressBar.at = mn
         ProgressBar.start = time.time()
         ProgressBar.last = 0
+        ProgressBar.curw = 0
         # sys.stderr.write('\n')
         ProgressBar.progress(mn)
 
     @staticmethod
-    def progress(prg=None, fin=False):
-        if prg is not None:
-            ProgressBar.at = prg
-        else:
-            ProgressBar.at = ProgressBar.at + 1
-            prg = ProgressBar.at
-        curt = time.time()
-        if curt - ProgressBar.last < 0.5 and not fin:
-            return
-        ProgressBar.last = curt
+    def clear():
+        sys.stderr.write('\r')
+        sys.stderr.write(' ' * ProgressBar.curw)
+        sys.stderr.write('\r')
+
+    @staticmethod
+    def draw(fin=False):
         # sys.stderr.write('\033[1F')
         sys.stderr.write('\r')
         width = 50
-        prog = 1 if ProgressBar.mn == ProgressBar.mx else float(prg - ProgressBar.mn) / (ProgressBar.mx - ProgressBar.mn)
+        prog = 1 if ProgressBar.mn == ProgressBar.mx else float(ProgressBar.at - ProgressBar.mn) / (ProgressBar.mx - ProgressBar.mn)
         bars = int(round(prog * width))
         bars = max(0, min(width, bars))
-        sys.stderr.write('%3d%% [%s%s] ' % (round(prog * 100), '#' * bars, '-' * (width - bars)))
-        elapsed = curt - ProgressBar.start
+        here = '%3d%% [%s%s] ' % (round(prog * 100), '#' * bars, '-' * (width - bars))
+        ProgressBar.curw = len(here)
+        sys.stderr.write(here)
+        elapsed = ProgressBar.last - ProgressBar.start
         # if elapsed >= 4 and prog > 0:
         show_time = None
         if fin:
@@ -45,8 +45,23 @@ class ProgressBar(object):
             m = math.floor(show_time / 60)
             show_time -= m * 60
             s = math.floor(show_time)
-            sys.stderr.write(' %02d:%02d:%02d' % (h,m,s))
+            here = ' %02d:%02d:%02d' % (h,m,s)
+            sys.stderr.write(here)
+            ProgressBar.curw += len(here)
         # sys.stderr.write('\n')
+
+    @staticmethod
+    def progress(prg=None, fin=False):
+        if prg is not None:
+            ProgressBar.at = prg
+        else:
+            ProgressBar.at = ProgressBar.at + 1
+            prg = ProgressBar.at
+        curt = time.time()
+        if curt - ProgressBar.last < 0.5 and not fin:
+            return
+        ProgressBar.last = curt
+        ProgressBar.draw(fin)
 
     @staticmethod
     def finish():
