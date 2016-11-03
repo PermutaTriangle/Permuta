ebdfd07326eb555393358d3fb7e0c4b0ec8e8198
RagnarArdal <RagnarArdal@gmail.com>
2016-11-03 16:56:50 +0000

    Some tests for rotate_left, not that fancy.

--- tests/test_permutation.py
+++ tests/test_permutation.py
@@ -90,7 +90,16 @@ class TestPermutation(unittest.TestCase):
             self.assertEqual(Permutation(list(range(i-1,0,-1))), Permutation(list(range(1,i))).rotate_right())
         self.assertEqual(Permutation([3,2,4,5,1,6,7]), Permutation([7,6,4,3,1,2,5]).rotate_right())
         self.assertEqual(Permutation([5,6,4,2,8,1,3,7]), Permutation([5,8,2,1,3,7,4,6]).rotate_right())
-        self.assertEqual(Permutation([1,2,3]), Permutation([3,2,1]).rotate_right())
+        self.assertEqual(Permutation([1,2,3]), Permutation([3,2,1]).rotate_right(5))
+        self.assertEqual(Permutation([5,6,4,2,1,3]), Permutation([5,6,4,2,1,3]).rotate_right(4))
+
+    def test_rotate_left(self):
+        for i in range(10):
+            self.assertEqual(Permutation(list(range(i-1,0,-1))), Permutation(list(range(1,i))).rotate_right())
+        self.assertEqual(Permutation([7,6,4,3,1,2,5]).rotate_left(), Permutation([7,6,4,3,1,2,5]).rotate_right(3))
+        self.assertEqual(Permutation([7,6,4,3,1,2,5]).rotate_left(), Permutation([7,6,4,3,1,2,5]).rotate_right(-1))
+        self.assertEqual(Permutation([5,8,2,1,3,7,4,6]).rotate_left(), Permutation([5,8,2,1,3,7,4,6]).rotate_right(7))
+        self.assertEqual(Permutation([]).rotate_left(), Permutation([]).rotate_left(123))
 
     def test_shift_left(self):
         self.assertEqual(Permutation([]), Permutation([]).shift_left())
