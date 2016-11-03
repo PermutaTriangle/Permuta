cc274c3e60520fc0c4b088bd57cd683a52f529a0
RagnarArdal <RagnarArdal@gmail.com>
2016-11-03 11:59:02 +0000

    The 'ranges' experiment did not yield any performance gains, but the functions now remain available in permuta.misc. Scrap them or nuh?

--- permuta/permutation.py
+++ permuta/permutation.py
@@ -1,19 +1,10 @@
 from permuta.misc import left_floor_and_ceiling
-#from permuta.misc import cyclic_range
 import collections
 import itertools
 import numbers
 import sys
 if sys.version_info.major is 2:
     range = xrange
-
-def cyclic_range(start, end, restart):
-    """Yields start,...,end-1,restart,...,start-1."""
-    return itertools.chain(range(start, end), range(restart, start))
-
-def modulo_range(start, modulo):
-    """Yields start,...,modulo-1,0,...,start-1."""
-    return cyclic_range(start, modulo, 0)
 
 class Permutation(object):
     """A permutation class.
