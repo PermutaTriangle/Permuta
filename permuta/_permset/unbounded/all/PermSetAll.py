52503c7898bbf5cd9647ed05341516cde677d2da
RagnarArdal <RagnarArdal@gmail.com>
2017-01-11 16:32:25 +0000

    Faffing about in all permutations set

--- permuta/_permset/unbounded/all/PermSetAll.py
+++ permuta/_permset/unbounded/all/PermSetAll.py
@@ -1,4 +1,5 @@
-import collections
+# TODO: Module docstring
+
 import itertools
 import random
 import sys
@@ -6,28 +7,27 @@ import sys
 from math import factorial
 
 from permuta import Perm
-from permuta.math import catalan
+from permuta._permset import PermSetBase
+from permuta._permset.unbounded import PermSetUnbounded
 
 
 if sys.version_info.major == 2:
     range = xrange  # pylint: disable=redefined-builtin,invalid-name,undefined-variable
 
 
-from ..PermSetUnbounded import PermSetUnbounded
-
-
-class PermSetAll(PermSetUnbounded):
+class PermSetAll(PermSetUnbounded):  # TODO: docstring
+    __PERM_SET_CACHE = {}
     def contains(self, perm):
         raise NotImplementedError
     def up_to(self, perm):
         raise NotImplementedError
     def __getitem__(self, key):
-        return PermSetAllSpecificLength(key)
+        return PermSetDispatcher(key)
     def __repr__(self):
         return "<The set of all perms>"
 
 
-class PermSetAllSpecificLength(itertools.permutations):
+class PermSetAllSpecificLength(PermSetBase, itertools.permutations):  # TODO: Inherit from proper superclass
     """Class for iterating through all perms of a specific length."""
 
     __slots__ = ("length", "domain")
@@ -39,9 +39,14 @@ class PermSetAllSpecificLength(itertools.permutations):
         instance.domain = domain
         instance.length = length
         return instance
-    
+
     def is_polynomial(self):
         return False
+
+    def random(self):
+        """Return a random perm of the length."""
+        random.shuffle(self.domain)  # TODO: Not use domain attribute?
+        return Perm(self.domain)
 
     if sys.version_info.major == 2:
         def next(self):
@@ -50,13 +55,9 @@ class PermSetAllSpecificLength(itertools.permutations):
         def __next__(self):
             return Perm(super(PermSetAllSpecificLength, self).__next__())  # pylint: disable=no-member
 
-    def __iter__(self):
-        return self
-
-    def random(self):
-        """Return a random perm of the length."""
-        random.shuffle(self.domain)  # TODO: Not use domain attribute?
-        return Permutation(self.domain)
+    def __contains__(self, other):
+        """Check if other is a permutation in the set."""
+        return isinstance(other, Permutation) and len(other) == self.length
 
     def __len__(self):
         return factorial(self.length)
@@ -64,9 +65,5 @@ class PermSetAllSpecificLength(itertools.permutations):
     def __str__(self):
         return "The set of all perms of length {}".format(self.length)
 
-    def __str__(self):
-        return "<PermSet of all perms of length {}".format(self.length)
-
-    def __contains__(self, other):
-        """Check if other is a permutation in the set."""
-        return isinstance(other, Permutation) and len(other) == self.length
+    def __repr__(self):
+        return "<PermSet of all perms of length {}>".format(self.length)
