24d56777ab5040c4b51b8e3b31cf5ee016aef0d8
Tomas Ken <tomkenmag@gmail.com>
2017-02-01 13:24:00 +0000

    imported gen_meshpatts in __init__

--- permuta/__init__.py
+++ permuta/__init__.py
@@ -1,6 +1,6 @@
 from .Perm import Perm
 from .PermSet import PermSet
-from .MeshPatt import MeshPatt
+from .MeshPatt import MeshPatt, gen_meshpatts
 from . import _perm_set
 from . import descriptors
 from . import interfaces
