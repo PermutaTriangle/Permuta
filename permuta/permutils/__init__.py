e94c4fe29351da258be7b02f8b9cd828b47c0369
Christian Bean <christianbean@ru.is>
2017-04-02 09:11:47 +0000

    added polynomial and regular insertion encodable to permutils

--- permuta/permutils/__init__.py
+++ permuta/permutils/__init__.py
@@ -1,1 +1,3 @@
 from .symmetry import *
+from .polynomial import is_polynomial, is_non_polynomial
+from .insertion_encodable import is_insertion_encodable_rightmost, is_insertion_encodable_maximum, is_insertion_encodable
---
+++ permuta/permutils/insertion_encodable.py
@@ -0,0 +1,80 @@
+from permuta import Perm
+
+def is_incr_next_incr(perm):
+    for i in range(len(perm) - 1):
+        if perm[i+1] < perm[i]:
+            for j in range(i+1,len(perm) - 1):
+                if perm[j+1] < perm[j]:
+                    return False
+            break
+    return True
+
+def is_incr_next_decr(perm):
+    for i in range(len(perm) - 1):
+        if perm[i+1] < perm[i]:
+            for j in range(i+1,len(perm) - 1):
+                if perm[j+1] > perm[j]:
+                    return False
+            break
+    return True
+
+def is_decr_next_incr(perm):
+    for i in range(len(perm) - 1):
+        if perm[i+1] > perm[i]:
+            for j in range(i+1,len(perm) - 1):
+                if perm[j+1] < perm[j]:
+                    return False
+            break
+    return True
+
+def is_decr_next_decr(perm):
+    for i in range(len(perm) - 1):
+        if perm[i+1] > perm[i]:
+            for j in range(i+1,len(perm) - 1):
+                if perm[j+1] > perm[j]:
+                    return False
+            break
+    return True
+
+mem = dict()
+def insertion_encodable_properties(perm):
+    if perm in mem:
+        return mem[tuple(perm)]
+
+    properties = []
+    if is_incr_next_decr(perm):
+        properties.append(0)
+    if is_incr_next_incr(perm):
+        properties.append(1)
+    if is_decr_next_decr(perm):
+        properties.append(2)
+    if is_decr_next_incr(perm):
+        properties.append(3)
+
+    res = sum( 1 << x for x in properties )
+    mem[perm] = res
+
+    return res
+
+def is_insertion_encodable_rightmost(basis):
+    goal = (1 << 4) - 1
+    curr = 0
+    # Check for insertion_encodable by rightmost
+    for perm in basis:
+        curr = curr | insertion_encodable_properties(perm)
+        if curr == goal:
+            return True
+    return False
+
+def is_insertion_encodable_maximum(basis):
+    goal = (1 << 4) - 1
+    curr = 0
+    # Check for insertion_encodable by maximum
+    for perm in basis:
+        curr = curr | insertion_encodable_properties(perm.rotate())
+        if curr == goal:
+            return True
+    return False
+
+def is_insertion_encodable(basis):
+    return is_insertion_encodable_rightmost(basis) or is_insertion_encodable_maximum(basis)
