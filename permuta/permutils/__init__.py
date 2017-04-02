7b9776dc065d83209cacece4b578c08062fe5fb3
Christian Bean <christianbean@ru.is>
2017-04-02 10:15:29 +0000

    is_finite in permutils

--- permuta/permutils/__init__.py
+++ permuta/permutils/__init__.py
@@ -1,3 +1,4 @@
 from .symmetry import *
 from .polynomial import is_polynomial, is_non_polynomial
 from .insertion_encodable import is_insertion_encodable_rightmost, is_insertion_encodable_maximum, is_insertion_encodable
+from .finite import is_finite
