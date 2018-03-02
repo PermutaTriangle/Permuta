29773253760b42da24dd5a5bb5b6f400c850864a
Tomas Ken <tomkenmag@gmail.com>
2018-03-02 14:55:06 +0000

    Removed unused avoidance classes

--- permuta/_perm_set/unbounded/described/avoiding/avoiding_length_3/CatalanAvoidingSpecificLength.py
+++
@@ -1,8 +0,0 @@
-from .Avoiding import AvoidingSpecificLength
-
-from permuta.misc import catalan
-
-
-class CatalanAvoidingSpecificLength(AvoidingSpecificLength):
-    def __len__(self):
-        return catalan(self.length)
