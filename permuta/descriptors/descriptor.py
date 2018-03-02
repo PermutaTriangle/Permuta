36502a31aa49a4cf53abf013b93e900c650333e2
Tomas Ken <tomkenmag@gmail.com>
2018-03-02 14:33:15 +0000

    descriptors PEP8

--- permuta/descriptors/descriptor.py
+++ permuta/descriptors/descriptor.py
@@ -12,18 +12,3 @@ class Descriptor(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def __hash__(self):
         pass
-
-
-#class ContainmentBasis(Descriptor):
-#    def __init__(self, n, o):
-#        self.n = n
-#        self.o = o
-#    def __eq__(self, other):
-#        return isinstance(other, self.__class__) and self.n == other.n and self.o == other.o
-#
-#
-#class Predicate(Descriptor):
-#    def __init__(self, predicate):
-#        self.predicate = predicate
-#    def __eq__(self, other):
-#        return isinstance(other, self.__class__) and self.predicate == other.predicate
