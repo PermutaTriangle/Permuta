bb8909665749735ea4899c1c72842a40ddfe02c3
Tomas Ken <tomkenmag@gmail.com>
2018-03-05 16:13:12 +0000

    Moved from markdown to rest for pypi and added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -17,12 +17,31 @@ setup(
     keywords=("permutation perm mesh pattern patt avoid contain occurrence"
               "statistic"),
     url="https://github.com/PermutaTriangle/Permuta",
+    project_urls={
+        'Source': 'https://github.com/PermutaTriangle/Permuta',
+        'Tracker': 'https://github.com/PermutaTriangle/Permuta/issues'
+    },
     packages=find_packages(),
-    long_description=read("README.md"),
+    long_description=read("README.rst"),
     extras_require={"plot": ["seaborn"]},
     setup_requires=["pytest-runner"],
     tests_require=["pytest",
                    "permuta",
                    "pytest-pep8",
                    "pytest-isort"],
+    python_requires='>=3.4',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
 )
