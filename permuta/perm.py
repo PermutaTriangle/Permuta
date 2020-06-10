3121dad28a99343addf3a0f179b6736711dd7df7
JonSteinn <jonstienn@gmail.com>
2020-06-10 08:06:27 -0400

    improve var names

--- permuta/perm.py
+++ permuta/perm.py
@@ -1202,16 +1202,16 @@ class Perm(tuple, Patt, Rotatable, Shiftable, Flippable):
             >>> Perm.monotone_increasing(7).count_inversions()
             0
         """
-        l = len(self) + 1
-        bit = [0] * l
-        for x in reversed(self):
-            i = x + 1
-            while x:
-                bit[0] += bit[x]
-                x &= x - 1
-            while i < l:
-                bit[i] += 1 
-                i += i & -i
+        bit_len = len(self) + 1
+        bit = [0] * bit_len
+        for element in reversed(self):
+            bit_index = element + 1
+            while element:
+                bit[0] += bit[element]
+                element &= element - 1
+            while bit_index < bit_len:
+                bit[bit_index] += 1 
+                bit_index += bit_index & -bit_index
         return bit[0]
 
     def inversions(self):
