7d26cbb03cab91e38106b2c12caba9ecdd104421
Christian Bean <christianbean@ru.is>
2020-06-12 13:15:50 +0200

    import lex min

--- permuta/permutils/__init__.py
+++ permuta/permutils/__init__.py
@@ -3,9 +3,11 @@ from .insertion_encodable import (
     is_insertion_encodable_maximum,
     is_insertion_encodable_rightmost,
 )
+from .symmetry import lex_min
 
 __all__ = [
     "is_insertion_encodable",
     "is_insertion_encodable_maximum",
     "is_insertion_encodable_rightmost",
+    "lex_min",
 ]
