a5b390d6c96fabef7c8b3e4cf627452a6fc164aa
Émile Nadeau <nadeau.emile@gmail.com>
2020-06-17 05:47:02 -0400

    version 1.4.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="permuta",
-    version="1.4.1",
+    version="1.4.2",
     author="Permuta Triangle",
     author_email="permutatriangle@gmail.com",
     description="A comprehensive high performance permutation library.",
