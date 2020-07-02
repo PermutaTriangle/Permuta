ef4122aa383bda97a0c8e6573211bcc8c9ed3e54
JonSteinn <jonsteinn@gmail.com>
2020-07-01 21:03:45 -0400

    mypy sympy

--- permuta/interfaces/patt.py
+++ permuta/interfaces/patt.py
@@ -1,4 +1,5 @@
 import abc
+from typing import Iterator, List, Tuple, Union
 
 ABC = abc.ABCMeta("ABC", (object,), {})
 
@@ -32,7 +33,7 @@ class Patt(ABC):
         """
         return all(self in perm for perm in perms)
 
-    def count_occurrences_in(self, perm):
+    def count_occurrences_in(self, perm) -> int:
         """Count the number of occurrences of self in perm.
 
         Args:
@@ -47,6 +48,8 @@ class Patt(ABC):
         return sum(1 for _ in self.occurrences_in(perm))
 
     @abc.abstractmethod
-    def occurrences_in(self, perm):
+    def occurrences_in(
+        self, perm
+    ) -> Union[Iterator[List[Tuple[int]]], Iterator[Tuple[()]]]:
         """Find all indices of occurrences of self in perm."""
         pass
