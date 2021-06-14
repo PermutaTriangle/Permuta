c43e5e1afdc7457e23655579dbe7fd1bec72af17
Émile Nadeau <nadeau.emile@gmail.com>
2021-06-14 09:35:45 -0400

    version 2.1.0 (#174)

--- permuta/__init__.py
+++ permuta/__init__.py
@@ -1,7 +1,7 @@
 from .patterns import BivincularPatt, CovincularPatt, MeshPatt, Perm, VincularPatt
 from .perm_sets.permset import Av, Basis, MeshBasis
 
-__version__ = "2.0.4"
+__version__ = "2.1.0"
 
 __all__ = [
     "Perm",
